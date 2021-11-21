aa7ea91419279b71651a46bde87bfe23c6a0c79d
vitalik <vitalik142003@gmail.com>
2021-11-21 14:55:02 +0300

    Initial

---
+++ src/main/java/com/task/footbalteam/FootbalteamApplication.java
@@ -0,0 +1,13 @@
+package com.task.footbalteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootbalteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/footbalteam/FootbalteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.footbalteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbalteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
